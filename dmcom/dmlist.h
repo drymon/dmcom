2ff4bbe07904c4ff76708fb48b0a8d130196f195
nam <nguyenvannam142@gmail.com>
2020-03-16 19:46:05 +0900

    Changed dmlist member name

--- dmcom/dmlist.h
+++ dmcom/dmlist.h
@@ -10,33 +10,33 @@ struct dmnode {
 };
 
 struct dmlist {
-	struct dmnode node;
-	int nelems;
+	struct dmnode anchor;
+	int num_nodes;
 };
 
 static inline void dmlist_init(struct dmlist *list)
 {
-	list->node.next = &list->node;
-	list->node.prev = &list->node;
-	list->nelems = 0;
+	list->anchor.next = &list->anchor;
+	list->anchor.prev = &list->anchor;
+	list->num_nodes = 0;
 }
 
 static inline void dmlist_add_tail(struct dmlist *list, struct dmnode *node)
 {
-	node->prev = list->node.prev;
-	list->node.prev->next = node;
-	node->next = &list->node;
-	list->node.prev = node;
-	list->nelems++;
+	node->prev = list->anchor.prev;
+	list->anchor.prev->next = node;
+	node->next = &list->anchor;
+	list->anchor.prev = node;
+	list->num_nodes++;
 }
 
 static inline void dmlist_add_head(struct dmlist *list, struct dmnode *node)
 {
-	node->next = list->node.next;
-	node->prev = &list->node;
-	list->node.next->prev = node;
-	list->node.next = node;
-	list->nelems++;
+	node->next = list->anchor.next;
+	node->prev = &list->anchor;
+	list->anchor.next->prev = node;
+	list->anchor.next = node;
+	list->num_nodes++;
 }
 
 static inline void dmlist_add_before(struct dmlist *list, struct dmnode *where, struct dmnode *node)
@@ -45,7 +45,7 @@ static inline void dmlist_add_before(struct dmlist *list, struct dmnode *where, 
 	node->next = where;
 	where->prev->next = node;
 	where->prev = node;
-	list->nelems++;
+	list->num_nodes++;
 }
 
 static inline void dmlist_add_after(struct dmlist *list, struct dmnode *where, struct dmnode *node)
@@ -54,31 +54,31 @@ static inline void dmlist_add_after(struct dmlist *list, struct dmnode *where, s
 	node->next = where->next;
 	where->next->prev = node;
 	where->next = node;
-	list->nelems++;
+	list->num_nodes++;
 }
 
 static inline bool dmlist_is_empty(struct dmlist *list)
 {
-	return (list->nelems == 0);
+	return (list->num_nodes == 0);
 }
 
 static inline struct dmnode *dmlist_get_head(struct dmlist *list)
 {
 	if(dmlist_is_empty(list))
 		return NULL;
-	return list->node.next;
+	return list->anchor.next;
 }
 
 static inline struct dmnode *dmlist_get_tail(struct dmlist *list)
 {
 	if(dmlist_is_empty(list))
 		return NULL;
-	return list->node.prev;
+	return list->anchor.prev;
 }
 
-static inline int dmlist_get_nelems(struct dmlist *list)
+static inline int dmlist_get_num_nodes(struct dmlist *list)
 {
-	return list->nelems;
+	return list->num_nodes;
 }
 
 static inline void dmlist_merge(struct dmlist *list, struct dmlist *merge)
@@ -86,11 +86,11 @@ static inline void dmlist_merge(struct dmlist *list, struct dmlist *merge)
 	if(dmlist_is_empty(merge))
 		return;
 	
-	list->node.prev->next = merge->node.next;
-	merge->node.next->prev = list->node.prev;
-	merge->node.prev->next = &list->node;
-	list->node.prev = merge->node.next;
-	list->nelems += merge->nelems;
+	list->anchor.prev->next = merge->anchor.next;
+	merge->anchor.next->prev = list->anchor.prev;
+	merge->anchor.prev->next = &list->anchor;
+	list->anchor.prev = merge->anchor.next;
+	list->num_nodes += merge->num_nodes;
 	dmlist_init(merge);
 }
 
@@ -99,16 +99,16 @@ static inline void dmlist_remove(struct dmlist *list, struct dmnode *node)
 	node->next->prev = node->prev;
 	node->prev->next = node->next;
 	node->next = node->prev = NULL;
-	list->nelems--;
+	list->num_nodes--;
 }
 
 #define DMLIST_ENTRY(ptr, type, member)\
 	((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
 
 #define DMLIST_FOREACH(list, node)\
-	for(node=(list)->node.next; node!=&(list)->node; node=node->next)
+	for(node=(list)->anchor.next; node!=&(list)->anchor; node=node->next)
 
 #define DMLIST_FOREACH_SAFE(list, node, tmp)\
-	for(node=(list)->node.next, tmp=node->next; node!=&(list)->node; node=tmp,tmp=node->next)
+	for(node=(list)->anchor.next, tmp=node->next; node!=&(list)->anchor; node=tmp,tmp=node->next)
 
 #endif /*__DMLIST_H_*/
